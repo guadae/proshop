68804095958d4b75fe390514f10b7496dd3ca8a1
Erica Guadalupe <erica.fguadalu@gmail.com>
2023-11-26 21:06:44 -0500

    added additional error handling

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,24 @@
+// if no other are no other middleware that can handle the request
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// overwrite the default express error handler
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message; 
+
+  //check for Mongoose bad id / CastError
+  if(err.name === 'CastError' && err.kind === 'Object Od') {
+    message = `Resource not found`;
+    statusCode = 404; 
+  }
+  res.status(statusCode).json({
+    message, 
+    stack: process.env.NODE_ENV === 'production' ? 'stack of pancakes here yo' : err.stack,
+  });
+}
+
+export { notFound, errorHandler }; 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
 // establish get request from /api/products to return a response with the products in json format
-router.get('', asyncHandler(async (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 }))
@@ -16,7 +16,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
   if(product) {
     return res.json(product);
   } 
-  res.status(404).json({ message: 'Product not found'})
+  res.status(404);
+  throw new Error('Resource not found');
 }))
 
 export default router;
