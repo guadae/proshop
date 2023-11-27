98753e68bfa067de00f3f826f2d7164e355f2f00
Erica Guadalupe <erica.fguadalu@gmail.com>
2023-11-26 22:01:28 -0500

    created controllers folder for organization

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,8 @@
 import express from 'express';
 const router = express.Router(); 
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import { getProducts, getProductById } from '../controllers/productController.js';
 
-// establish get request from /api/products to return a response with the products in json format
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-}))
-
-// get individual product pages
-router.get('/:id', asyncHandler(async (req, res) => {
-  // const product = products.find((p) => p._id === req.params.id);
-  const product = await Product.findById(req.params.id);
-  if(product) {
-    return res.json(product);
-  } 
-  res.status(404);
-  throw new Error('Resource not found');
-}))
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
