d3c2d6a9946c1229540802d2abe4f902b2c29254
Erica Guadalupe <erica.fguadalu@gmail.com>
2023-12-04 20:58:29 -0500

    replaced axios connection to backend with redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,28 +3,23 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({}); 
 
   const { id: productId } = useParams();
 
-  useEffect(() => {
-      const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data)
-      }
-      fetchProduct();
-  }, [productId])
-
-  // const product = products.find((p) => p._id === productId);
-
-  // console.log(product)
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link to="/" className="btn btn-light my-3">Go Back</Link>
+        
+    {isLoading ? (
+      <div>Loading...</div>
+    ) : error ? (
+      <div>{ error?.data.message || error.error }</div>
+    ) : (
       <Row>
       <Col md={5}>
         <Image src={product.image} alt={product.name} fluid/>
@@ -66,8 +61,8 @@ const ProductScreen = () => {
           </ListGroup>
         </Card>
       </Col>
-        
       </Row>
+      )}
     </>
   )
 }
